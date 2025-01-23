5f19ed16381d589e5f7ad76f8794b98df8158ada
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-01-23 12:16:12 +0100

    Practica1 funcional

--- Practica1/src/aplicacion/Principal.java
+++ Practica1/src/aplicacion/Principal.java
@@ -4,7 +4,7 @@ import mates.Matematicas;
 
 public class Principal{
     public static void main(String[] args){
-        System.out.println("El número PI es " +
+        System.out.println("El numero PI es " +
                 Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
     }
 }
--- Practica1/src/mates/Matematicas.java
+++ Practica1/src/mates/Matematicas.java
@@ -7,6 +7,14 @@ public class Matematicas{
      * * generado.
      * */
     public static double generarNumeroPiIterativo(long pasos){
-        return 0; // Este código hay que cambiarlo.
+    int puntos = 0;
+        for (int i=0; i<pasos; i++){
+            double x = Math.random();
+            double y = Math.random();
+            if (x*x + y*y < 1){
+                puntos++;
+            }
+        }
+        return 4.0 * puntos / pasos;
     }
 }
