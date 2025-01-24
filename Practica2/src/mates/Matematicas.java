4b8525681f981d07c3c62f69ff33f4d6f44b99a4
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-01-24 09:15:33 +0100

    Comprobacion de argumentos ilegales y metodo auxliar para pedir solo un argumento

--- Practica2/src/mates/Matematicas.java
+++ Practica2/src/mates/Matematicas.java
@@ -6,17 +6,24 @@ public class Matematicas{
      * * Montecarlo. El parámetro `pasos` indica el número de puntos
      * * generado.
      * */
-    public static double generarNumeroPiIterativo(long pasos,long contador){
-        
-        if(contador== 0){
+    public static double generarNumeroPiRecursivo(long pasos){
+        if(pasos<0){
+            throw new IllegalArgumentException("El numero de pasos debe ser positivo o 0");
+        }
+        return auxiliarNumeroPiRecursivo(pasos,pasos);
+    }
+    private static double auxiliarNumeroPiRecursivo(long pasos,long contador){
+       
+        if(contador == 0){
             return 0;
         }
         double x = Math.random();
         double y = Math.random();
         if(x*x+y*y<=1){
-            return (double) 4/pasos + generarNumeroPiIterativo(pasos,contador-1);
+            return (double) 4/pasos + auxiliarNumeroPiRecursivo(pasos,contador-1);
         }else{
-            return generarNumeroPiIterativo(pasos,contador-1);
+            return auxiliarNumeroPiRecursivo(pasos,contador-1);
         }
-    }
+    }  
+    
 }
