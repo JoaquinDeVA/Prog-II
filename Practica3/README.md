6ac662175b1aab168970cb9060ce579e058fa3cd
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-01-24 09:16:05 +0100

    Archivos base practica3

---
+++ Practica3/README.md
@@ -0,0 +1,4 @@
+Programa iterativo que permite obtener una aproximacion al número pi mediante el método Montecarlo a través de expresiones lambda.
+
+Joaquin de Vicente Abad.
+
---
+++ Practica3/src/aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package aplicacion;
+
+import mates.Matematicas;
+
+public class Principal{
+    public static void main(String[] args){
+        System.out.println("El numero PI es " +
+                Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
+    }
+}
