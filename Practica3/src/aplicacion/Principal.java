97584c6f0cf76a40d6f7bd7204f6f47b9105620a
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-03-08 10:22:28 +0100

    Practica 3 completa

--- Practica3/src/aplicacion/Principal.java
+++ Practica3/src/aplicacion/Principal.java
@@ -3,6 +3,7 @@ package aplicacion;
 import mates.Matematicas;
 
 public class Principal{
+    /** Toma como argumento de programa el numero de pasos y procede a imprimir por pantalla el resultado **/
     public static void main(String[] args){
         System.out.println("El numero PI es " +
                 Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
