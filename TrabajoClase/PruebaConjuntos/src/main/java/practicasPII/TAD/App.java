e46544509e41126ff590b19f5d88761b990d696e
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-03-21 13:16:02 +0100

    Trabajo de clase. Pruebas con maven

---
+++ TrabajoClase/PruebaConjuntos/src/main/java/practicasPII/TAD/App.java
@@ -0,0 +1,32 @@
+package practicasPII.TAD;
+
+import java.util.*;
+
+public class App{
+    public static boolean feliz(int n){
+        
+        HashSet<Integer> c=new HashSet<Integer>();  
+        int suma=0, d;
+        System.out.print(n) ;
+       
+        while (n!=1 && !c.contains((Integer) n)){
+            
+            c.add((Integer) n);
+            suma=0;
+            while (n>0){
+                
+                d=n%10;
+                suma+=d*d;
+                n/=10;
+            }
+        n=suma;
+        System.out.print ("-->"+n);
+        }
+        return n==1;
+    }
+    public static void main( String[] args ){
+    
+        int n=45;
+        System.out.println( " El número "+n+" es feliz: "+feliz(n) );
+    }
+}
