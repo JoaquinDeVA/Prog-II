158aff61c65a9444b7fc3b74a3cb82296254418d
Joaquin De Vicente Abad <joaquin.devicenteabad@usp.ceu.es>
2025-04-02 12:34:14 +0200

    Practica 4

---
+++ Practica4/src/test/java/aplicacion/PrincipalTest.java
@@ -0,0 +1,14 @@
+package aplicacion;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PrincipalTest {
+ 
+    @Test
+    public void pruebaValida(){
+
+
+    }
+}
